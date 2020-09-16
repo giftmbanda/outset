edafd2625b72dca582f88c582dc381291487d1dc
Gift <giftmbanda@gmail.com>
2020-09-17 01:27:44 +0200

    make it better

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./App.css"; //Import here your file style
-import Footer from "./components/Footer";
-import Checkout from "./components/Checkout";
 import ItemCard from "./components/ItemCard";
-import SignInSide from "./components/SignInSide";
-import SignUp from "./components/SignUp";
-import NavBar from "./components/NavBar";
-
+// import Footer from "./components/Footer";
+// import Checkout from "./components/Checkout";
+// import SignInSide from "./components/SignInSide";
+// import NavBar from "./components/NavBar";
+// import SignUp from "./components/SignUp";
 
 const App = () => {
   return (
     <React.Fragment>
-      <SignUp />
+      <ItemCard />
     </React.Fragment>
   );
 };
